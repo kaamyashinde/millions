7b3e18b35d5ff55d19606cbdc54257f45cc96605
kaamyashinde <kamyaas@stud.ntnu.no>
2026-01-30 18:17:49 +0100

    feat: create stock class

---
+++ src/main/java/model/Stock.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a stock in a company. This has a unique stock symbol, f.e.g., "AAPL" for Apple Inc.
+ * The stock is sold on Exchanges for a price that updates weekly.
+ *
+ * @author kaamyashinde
+ * @version 0.0.1
+ * @since 30-01-2026
+ */
+
+// TODO: Refer to the exchanges class when it is implemented
+
+public class Stock {
+
+
+  private final String symbol;
+  private final String company;
+  private final ArrayList<BigDecimal> price;
+
+
+  public Stock(String symbol, String company, ArrayList<BigDecimal> price) {
+    this.symbol = symbol;
+    this.company = company;
+    this.price = price;
+  }
+
+
+  public String getSymbol() {
+    return symbol;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public List<BigDecimal> getPrice() {
+    return price;
+  }
+
+  @Override
+  public String toString() {
+    return "Stock{" +
+        "symbol='" + symbol + '\'' +
+        ", company='" + company + '\'' +
+        ", price=" + price +
+        '}';
+  }
+}
