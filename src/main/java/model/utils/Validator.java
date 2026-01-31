c5b94876e032396010f26eabeee0dc94fab3943a
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2026-01-31 18:22:45 +0100

    doc: validator throws exc

--- src/main/java/model/utils/Validator.java
+++ src/main/java/model/utils/Validator.java
@@ -13,7 +13,12 @@ public class Validator {
   private Validator() {
   }
 
-  //create a static method to check if an abject is null
+  /**
+   * create a static method to check if an abject is null
+   * @param obj The object to be checked
+   * @param obType The type of the object, friendly name for error message
+   * @throws IllegalArgumentException if the object is null
+   */
   public static void checkNotNull(Object obj, String obType) {
     if (obj == null) {
       throw new IllegalArgumentException(obType + " cannot be null");
