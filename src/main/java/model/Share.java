ff61dc18558c2277575559c813cb236192d11b15
Kaamya Shinde <kamyaas@stud.ntnu.no>
2026-01-31 17:50:55 +0100

    feat: error handling check in constructor

--- src/main/java/model/Share.java
+++ src/main/java/model/Share.java
@@ -1,13 +1,15 @@
 package model;
 
+import static model.utils.Validator.checkNotNull;
 import java.math.BigDecimal;
 
 /**
- * Represents a share in the stock market. A purchase results in acquiring certain amount of Shares.
- * @author kaamyashinde
- * @version 0.0.1
- * @since 31-01-2026
+ * Represents a share in the stock market. A purchase results in acquiring certain amount of
+ * Shares.
  *
+ * @author kaamyashinde
+ * @version 0.0.2
+ * @since 31-01-2026
  */
 
 public class Share {
@@ -18,11 +20,13 @@ public class Share {
 
   /**
    * This is the constructor for the Share class.
-   * @param stock The stock associated with the share.
-   * @param quantity The quantity of shares purchased.
+   *
+   * @param stock         The stock associated with the share.
+   * @param quantity      The quantity of shares purchased.
    * @param purchasePrice The price at which the shares were purchased.
    */
   public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice) {
+    checkNotNull(stock, "Stock cannot be null");
     this.stock = stock;
     this.quantity = quantity;
     this.purchasePrice = purchasePrice;
@@ -30,6 +34,7 @@ public class Share {
 
   /**
    * Gets the stock associated with the share.
+   *
    * @return The stock associated with the share.
    */
   public Stock getStock() {
@@ -38,6 +43,7 @@ public class Share {
 
   /**
    * Gets the quantity of shares purchased.
+   *
    * @return The quantity of shares purchased.
    */
   public BigDecimal getQuantity() {
@@ -46,6 +52,7 @@ public class Share {
 
   /**
    * Gets the price at which the shares were purchased.
+   *
    * @return The price at which the shares were purchased.
    */
   public BigDecimal getPurchasePrice() {
