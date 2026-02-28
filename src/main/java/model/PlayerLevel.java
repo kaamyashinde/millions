0382d6a47b492e4914aa2d6fc16eea75e551a02b
Kaamya Shinde <kamyaas@stud.ntnu.no>
2026-02-28 18:23:39 +0100

    feat: add player level enum

---
+++ src/main/java/model/PlayerLevel.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * An enum representing the different levels of players in the system.
+ *
+ * @author kaamyashinde
+ * @version 1.0.0
+ * @since 2026-02-28
+ */
+public enum PlayerLevel {
+  NOVICE,
+  INVESTOR,
+  SPECULATOR
+}
