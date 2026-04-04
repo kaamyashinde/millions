577543d1fb7f49afe1bc1ab6ff936d9f677137fc
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2026-04-04 16:06:41 +0200

    feat: add session service

---
+++ src/main/java/model/session/ActiveSession.java
@@ -0,0 +1,20 @@
+package model.session;
+
+import model.Exchange;
+import model.Player;
+
+/**
+ * In-memory representation of the currently logged-in user and their game state.
+ *
+ * @param username display username shown in the CLI
+ * @param normalizedUsername canonical username key used for persistence
+ * @param player active player state
+ * @param exchange active exchange state
+ */
+public record ActiveSession(
+    String username,
+    String normalizedUsername,
+    Player player,
+    Exchange exchange
+) {
+}
