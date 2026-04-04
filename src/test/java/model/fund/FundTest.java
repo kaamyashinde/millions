444ade53ec403228014154fe2f2028f624a48415
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2026-04-04 14:24:05 +0200

    feat: add fund asset model

---
+++ src/test/java/model/fund/FundTest.java
@@ -0,0 +1,26 @@
+package model.fund;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.math.BigDecimal;
+import java.util.List;
+import model.Stock;
+import org.junit.jupiter.api.Test;
+
+class FundTest {
+
+  @Test
+  void getSalesPrice_derivesValueFromUnderlyingStocks() {
+    Stock apple = new Stock("AAPL", "Apple Inc.");
+    apple.addNewSalesPrice(new BigDecimal("100.00"));
+    Stock microsoft = new Stock("MSFT", "Microsoft Corporation");
+    microsoft.addNewSalesPrice(new BigDecimal("200.00"));
+    Fund fund = new Fund(
+        "BLEND",
+        "Balanced Leaders Fund",
+        List.of(
+            new FundComponent(apple, new BigDecimal("0.25")),
+            new FundComponent(microsoft, new BigDecimal("0.75"))));
+
+    assertEquals(0, new BigDecimal("175.0000").compareTo(fund.getSalesPrice()));
+  }
+}
