e5f044b79f9a19c784f4354369cfb6b83adaa3f3
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2026-04-06 10:40:56 +0200

    fix: delete redundant HistoricalAssetPriceService.java

--- src/test/java/model/analysis/HistoricalAssetPriceServiceTest.java
+++ src/test/java/model/analysis/HistoricalAssetPriceServiceTest.java
@@ -3,7 +3,6 @@ package model.analysis;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.util.List;
-import model.core.market.HistoricalAssetPriceService;
 import model.core.market.fund.Fund;
 import model.core.market.fund.FundComponent;
 import model.core.market.stock.Stock;
