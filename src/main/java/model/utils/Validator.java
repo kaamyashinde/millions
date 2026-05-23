42f9ad2ab2caf2ec7945d6a01c3663b8e1d3d34e
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2026-05-23 16:25:42 +0200

    refactor: group utils together

--- src/main/java/model/utils/Validator.java
+++ src/main/java/model/utils/Validator.java
@@ -1,5 +1,6 @@
 package model.utils;
 
+
 import java.math.BigDecimal;
 
 /**
--- src/test/java/model/utils/ValidatorTest.java
+++ src/test/java/model/utils/ValidatorTest.java
@@ -1,5 +1,6 @@
 package model.utils;
 
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
